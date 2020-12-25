c35504376aec27f6f336e26cd37a40cd98d22527
lduranovic99 <luk.duranovic@gmail.com>
2020-12-25 13:56:20 +0100

    Isolating pages seems to work

--- main.hs
+++ main.hs
@@ -1,16 +1,24 @@
 import System.IO
+import Data.List
+import Data.Maybe
 
 main = do
     handle <- openFile "input.txt" ReadMode
-    -- How to get the type of this???
     contents <- hGetContents handle
     let pages = getPages contents
     print pages
 
-getPages :: String -> [String]
-getPages "" = []
+getPages                     :: String -> [String]
+getPages ""                  = []
 getPages s
-    | (length secondPart > 0) && (head . tail secondPart == '\n') = 
-        firstPart : getPages . dropWhile (== '\n') secondPart
-    | otherwise = s
-            where (firstPart, secondPart) = break (/= '\n') s
+    | emptyLineIndex == (-1) = [s]
+    | otherwise              = firstPart : getPages (dropWhile (== '\n') secondPart)
+        where firstPart      = take emptyLineIndex s
+              secondPart     = drop emptyLineIndex s
+              emptyLineIndex = findEmptyLineIndex  s
+
+findEmptyLineIndex :: String -> Int
+findEmptyLineIndex = (fromMaybe (-1)) . (getSubstringIndex "\n\n")
+
+getSubstringIndex :: String -> String -> Maybe Int
+getSubstringIndex y x = (y `isPrefixOf`) `findIndex` (tails x)
