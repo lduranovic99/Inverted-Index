8d8bfa2a63510de0939c28fb9ab7729cdc3e5e94
lduranovic99 <luk.duranovic@gmail.com>
2020-12-25 14:41:28 +0100

    Removed duplicate words from each page. Sorted each page

--- main.hs
+++ main.hs
@@ -1,12 +1,15 @@
 import System.IO
 import Data.List
 import Data.Maybe
+import Data.Char (isAlpha, toLower, isSpace)
 
 main = do
     handle <- openFile "input.txt" ReadMode
     contents <- hGetContents handle
-    let pages = getPages contents
-    print pages
+    let pages         = getPages contents
+        numberedPages = addNumbers pages
+        numberedWords = getWords numberedPages
+    print numberedWords
 
 getPages                     :: String -> [String]
 getPages ""                  = []
@@ -22,3 +25,18 @@ findEmptyLineIndex = (fromMaybe (-1)) . (getSubstringIndex "\n\n")
 
 getSubstringIndex :: String -> String -> Maybe Int
 getSubstringIndex y x = (y `isPrefixOf`) `findIndex` (tails x)
+
+
+addNumbers :: [String] -> [(Int, String)]
+addNumbers = zip [1..]
+
+
+getWords :: [(Int, String)] -> [(Int, [String])]
+getWords []           = []
+getWords ((n,str):xs) = (n, nub $ sort $ words $ removePunctuation str) : getWords xs
+
+removePunctuation :: String -> String
+removePunctuation [] = []
+removePunctuation (s:sx)
+    | (isAlpha s) || (isSpace s) = toLower s : removePunctuation sx
+    | otherwise = removePunctuation sx
